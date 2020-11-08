a0a0ea5a079ab9f27b07b77e1a3635b90925fe4d
ue8d <webmaster@tomoki12.com>
2020-11-08 17:04:56 +0900

    [add]投稿できるように

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
 end
