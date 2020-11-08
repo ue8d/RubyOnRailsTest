9b8ccad73cf4edd76c5560ee84e6b9d8819d2359
ue8d <webmaster@tomoki12.com>
2020-11-08 16:14:29 +0900

    [add]投稿用のルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
 end
