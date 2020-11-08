eb63272ac8b0564351b39cf77738f5ded3aa2258
ue8d <webmaster@tomoki12.com>
2020-11-08 14:40:57 +0900

    [add]postへのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about' => 'home#about'
+
+  get "posts/index" => "posts#index"
 end
