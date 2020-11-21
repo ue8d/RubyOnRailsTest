a39b8310aaa531352f5516ade248640da3f4de52
ue8d <webmaster@tomoki12.com>
2020-11-21 22:00:08 +0900

    ユーザ画像を表示する準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_171947) do
+ActiveRecord::Schema.define(version: 2020_11_21_125337) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_171947) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
